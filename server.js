27d242463febfc29bf9fab0e1f2ae8869bc5b233
PainCorner <141883811+PainCorner@users.noreply.github.com>
2023-12-16 20:48:53 +0900

    サーバー関連の変更

--- server.js
+++ server.js
@@ -1,10 +1,28 @@
-const express = require('express');
-const app = express();
+const http = require("http");
+const PORT = 8000;
+const html = require("fs").readFileSync("./index.html");
 
-app.length('/',(req,res) => {
-    res.send('Hello,World!');
+// サーバー作成
+const server = http.createServer((req,res) => {
+    // ブラウザからアクセスが来た時の処理
+    res.writeHead(200,{"Content-type":"text/html"});
+    res.write(html);
+    res.end();
+
 });
 
-app.listen(3001,() => {
-    console.log('サーバーがポート3000で起動しました。')
+server.listen(PORT,() =>{
+    console.log("server running!");
 });
+console.log('Hello Wolrd!');
+
+// const express = require('express');
+// const app = express();
+
+// app.length('/',(req,res) => {
+//     res.send('Hello,World!');
+// });
+
+// app.listen(3001,() => {
+//     console.log('サーバーがポート3000で起動しました。')
+// });
